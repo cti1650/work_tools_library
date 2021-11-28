76ea86dcdf6a5441bff4de6b6f243154f37612b1
cti1650 <cti1650@gmail.com>
2021-11-29 00:11:28 +0900

    ドキュメントの更新を反映

--- src/script.js
+++ src/script.js
@@ -24,7 +24,6 @@ function LineApp(token) {
 
 function TemplateDocs(folderId) {
   function docToDoc(tampFileId, data) {
-    //テンプレートファイルを読み込む
     const sourceDocument = DriveApp.getFileById(tampFileId);
     const originalName = sourceDocument.getName();
 
@@ -35,27 +34,53 @@ function TemplateDocs(folderId) {
 
     console.log("テンプレートファイルの名前：", sourceDocument.getName());
 
-    //フォルダを指定してファイルを複製
     const copyDir = DriveApp.getFolderById(folderId);
     const copyDocument = sourceDocument.makeCopy(fileName, copyDir);
     const fileId = copyDocument.getId();
 
-    //コピーしたドキュメントのURLを生成 シートなどに転記する際に便利
     const docUrl = "https://docs.google.com/document/d/" + fileId + "/edit";
     console.log("複製後のファイル URL：", docUrl);
 
-    //コピーしたドキュメントを開く
     const targetDocument = DocumentApp.openById(fileId);
     console.log(targetDocument.getName());
 
-    //シートの情報を流し込む
     let body = targetDocument.getBody();
     Object.keys(data).forEach((key) => {
       body.replaceText(`{${key}}`, data[key] || "");
     });
 
-    const array = [data, docUrl];
+    const array = { data, docUrl, fileId };
     return array;
   }
-  return { docToDoc };
+
+  function makePDF(fileId, fileName = "test") {
+    const des_url =
+      "https://docs.google.com/document/d/" + duplicateDocumentId + "/export?";
+    const opts = {
+      exportFormat: "pdf", // ファイル形式の指定 pdf / csv / xls / xlsx
+      format: "pdf", // ファイル形式の指定 pdf / csv / xls / xlsx
+      size: "A4", // 用紙サイズの指定 legal / letter / A4
+      portrait: "true", // true → 縦向き、false → 横向き
+      fitw: "true", // 幅を用紙に合わせるか
+      docNames: "false", // シート名を PDF 上部に表示するか
+      printtitle: "false", // スプレッドシート名を PDF 上部に表示するか
+      pagenumbers: "false", // ページ番号の有無
+      gridlines: "false", // グリッドラインの表示有無
+      fzr: "false", // 固定行の表示有無
+      // range :       'A1%3AA1',  // 対象範囲「%3A」 = : (コロン)
+    };
+    const urlExt = [];
+    for (optName in opts) {
+      urlExt.push(optName + "=" + opts[optName]);
+    }
+    const options = urlExt.join("&");
+    const token = ScriptApp.getOAuthToken();
+    const response = UrlFetchApp.fetch(des_url + options, {
+      headers: { Authorization: "Bearer " + token },
+    });
+    const blob = response.getBlob().setName(fileName + ".pdf");
+    var pdf_url = des_drive.createFile(blob).getUrl(); //　PDFを指定したフォルダに保存してURLを取得
+    return pdf_url;
+  }
+  return { docToDoc, makePDF };
 }
